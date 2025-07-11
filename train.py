994142ad60f436f30f3119f02408f4b2b640cba2
Miilee <yajursharma1234@gmail.com>
2025-07-11 20:31:36 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,6 @@ if __name__ == '__main__':
     from ultralytics import YOLO
     model = YOLO("yolov8.yaml")
     device = 'cuda:0'
-    results = model.train(data="config.yaml", epochs=5,device=device)
+    results = model.train(data="config.yaml", epochs=100,device=device)
 
 
