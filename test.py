2435578d24dff1f06b5723ef2544264db2a0fcc2
Miilee <yajursharma1234@gmail.com>
2025-07-11 19:13:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ if __name__ == '__main__':
     from ultralytics import YOLO
     model = YOLO(r"C:\Users\Miilee\PycharmProjects\test\funny\runs\detect\train\weights\best.pt")
     device = 'cuda:0'
-    results = model.predict(source=r"C:\Users\Ishan\Documents\test.mp4", save=True, conf=0.5)
+    results = model.predict(source=r"C:\Users\Miilee\Documents\test.mp4", save=True, conf=0.5)
 
